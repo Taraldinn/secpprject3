2c95d5682fd8442d1fc79d87f1937af1b5b79d1b
programmer-Quazi <kferdoush617@gmail.com>
2021-05-02 13:25:44 +0600

    ok

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'djangoProject4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join( BASE_DIR / 'templates')]
+        'DIRS': [ BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [BASE_DIR, 'static']
+STATIC_ROOT = BASE_DIR, 'static_root'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = BASE_DIR, '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
